60eba178abd9f65be3187bd9660e95bf53bd2b6f
Federico <federico.caiello@gmail.com>
2020-03-09 19:16:40 +0100

    end insert

--- update/server.php
+++ update/server.php
@@ -1,3 +1,48 @@
 <?php
   include_once __DIR__ . '/../env.php';
- ?>
+  include __DIR__ . '/../database.php';
+
+  if(empty($_POST['id'])) {
+    die('id non inserito');
+  }
+  if(empty($_POST['beds'])) {
+    die('non hai inserito il numero di letti');
+  }
+  if(empty($_POST['floor'])) {
+    die('non hai inserito il piano');
+  }
+  if(empty($_POST['room_number'])) {
+    die('non hai inserito il numero di stanza');
+  }
+
+
+  $idRoom = $_POST['id'];
+  $beds = $_POST['beds'];
+  $floor = $_POST['floor'];
+  $room_number = $_POST['room_number'];
+
+  $sql =
+  "SELECT *
+   FROM `stanze`
+   WHERE `id` = '$idRoom'";
+
+  $result = $conn->query($sql);
+
+  if ($result && $result->num_rows > 0) {
+    $room = $result->fetch_assoc();
+  }else {
+    die('id non esistente');
+  }
+
+  $sql =
+  "UPDATE `stanze`
+  SET `room_number` = $room_number, `beds` = $beds, `floor` = $floor
+  WHERE id = $idRoom";
+
+  $result = $conn->query($sql);
+
+  if($result) {
+    header("location:http://localhost/php-hotel-crud/show/show.php?id=$idRoom");
+  }else {
+    echo 'KO';
+  }
--- update/update.php
+++ update/update.php
@@ -1,6 +1,24 @@
 <?php
-  include 'server.php';
+  include __DIR__ . '/../database.php';
   include __DIR__ . '/../partials/header.php';
+
+  if (!empty($_GET['id'])) {
+    $idRoom = $_GET['id'];
+  }
+
+  $sql =
+  "SELECT *
+   FROM `stanze`
+   WHERE `id` = '$idRoom'";
+  $result = $conn->query($sql);
+
+  if ($result && $result->num_rows > 0) {
+    $room = $result->fetch_assoc();
+  }
+  else {
+    die('id non esistente');
+  }
+
  ?>
 
  <div class="container">
@@ -9,17 +27,17 @@
       <form action="server.php" method="POST">
         <div class="form-group">
           <label for="room_number">Numero Stanza</label>
-          <input class="form-control" type="text" name="room_number" value="">
+          <input class="form-control" type="text" name="room_number" value="<?php echo $room['room_number'] ?>">
         </div>
         <div class="form-group">
           <label for="floor">Piano</label>
-          <input type="text" name="room_number" value="">
+          <input type="text" name="floor" value="<?php echo $room['floor'] ?>">
         </div>
         <div class="form-group">
           <label for="beds">Numero Letti</label>
-          <input class="form-control" type="text" name="beds" value="">
+          <input class="form-control" type="text" name="beds" value="<?php echo $room['beds'] ?>">
           <div class="form-group">
-            <input type="hidden" name="id" value="">
+            <input type="hidden" name="id" value="<?php echo $room['id'] ?>">
             <input class="form-control" class="btn btn-submit" type="submit" value="salva">
 
           </div>
